7070f764a416b52ed86179f2f04df4c9329a655e
Owen TM <oturnermajor@gmail.com>
2016-03-31 15:32:43 +0100

    initial setup with nav bar

---
+++ tests/server.test.js
@@ -0,0 +1,26 @@
+const tape = require('tape');
+const server = require('../lib/server.js');
+
+tape('check that tests work!', (t) => {
+	t.equal(1, 1, 'success!');
+	t.end();
+});
+
+tape('check that server loads css', (t) => {
+	server.init.inject({ method: 'GET', url: '/style.css' }, (response) => {
+		t.ok(response.payload.indexOf('}') > -1, 'css loads properly');
+		t.end();
+	});
+});
+
+tape('check that server loads client side js', (t) => {
+	server.init.inject({ method: 'GET', url: '/script.js' }, (response) => {
+		t.ok(response.payload.indexOf('function') > -1, 'client side js loads properly');
+		t.end();
+	});
+});
+
+tape('teardown', (t) => {
+	server.init.stop();
+	t.end();
+});
